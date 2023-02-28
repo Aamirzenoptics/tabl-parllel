1fea5d116588230b9a3d586c020806980bee5c31
Aamirzenoptics <amir@zenoptics>
2023-02-28 15:12:31 +0530

    final flow 6

--- cypress/e2e/ip and dns.cy.js
+++ cypress/e2e/ip and dns.cy.js
@@ -4,13 +4,13 @@ describe('', () => {
         cy.exec('curl ifconfig.me').then(result => {
             const ipAddress = result.stdout.trim()
             // Log the IP address to the console
-            console.log(`Server IP address: ${ipAddress}`)
+            cy.log(`Server IP address: ${ipAddress}`)
           })
         
           cy.exec('nslookup $(hostname -i)').then(result => {
             const dnsName = result.stdout.trim().split('\n')[3].split(':')[1].trim()
             // Log the DNS name to the console
-            console.log(`Server DNS name: ${dnsName}`)
+            cy.log(`Server DNS name: ${dnsName}`)
           })
     });
     
